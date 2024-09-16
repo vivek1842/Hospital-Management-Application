db232af38df32e751fe35b3eb045ccd82b1fed33
vivek1842 <vivekghule2002@gmail.com>
2024-09-16 23:37:57 +0530

    new pages added

---
+++ src/pages/MyAppointments.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const MyAppointments = () => {
+  return (
+    <div>MyAppointments</div>
+  )
+}
+
+export default MyAppointments
---
+++ src/pages/MyProfile.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const MyProfile = () => {
+  return (
+    <div>MyProfile</div>
+  )
+}
+
+export default MyProfile
