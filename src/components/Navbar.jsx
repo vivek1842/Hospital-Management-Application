9e8783b20ad311999987e9a74eb097e06df370f4
vivek1842 <vivekghule2002@gmail.com>
2024-09-17 22:20:07 +0530

    navbar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,24 +5,24 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
-        <img src={assets.logo} alt="logo not found" />
-        <ul>
-            <NavLink>
-                <li>HOME</li>
-                <hr />
+    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
+        <img className='w-44 cursor-pointer' src={assets.logo} alt="logo not found" />
+        <ul className='hidden md:flex items-start gap-5 font-medium'>
+            <NavLink to='/'>
+                <li className='py-1'>HOME</li>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
             </NavLink>
-            <NavLink>
-                <li>ALL DOCTORS</li>
-                <hr />
+            <NavLink to='/doctors'>
+                <li className='py-1'>ALL DOCTORS</li>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
             </NavLink>
-            <NavLink>
-                <li>XABOUT</li>
-                <hr />
+            <NavLink to='/about'>
+                <li className='py-1'>ABOUT</li>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
             </NavLink>
-            <NavLink>
-                <li>CONTACT</li>
-                <hr />
+            <NavLink to='/contact'>
+                <li className='py-1'>CONTACT</li>
+                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
             </NavLink>
         </ul>
         <div>
